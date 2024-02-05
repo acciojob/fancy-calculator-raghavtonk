a3df7189c2115e7eaa52f976958fd883508fa364
Raghav Kumar Tonk <86098245+raghavtonk@users.noreply.github.com>
2024-02-05 23:19:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const handleResult = (fisrt,second)=>{
+	return("The final result is: "+(fisrt+second));
+}
+const first= promt("First Input");
+const second=promt("Second Input");
+alert(handleResult(first*2,second));
